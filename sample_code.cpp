57ce05da28e94242274380c6cc894a5a2e73ac06
Priyanshu Vaishnav <83324183+PranshVaishnav@users.noreply.github.com>
2025-06-25 12:04:33 +0530

    Update sample_code.cpp

--- sample_code.cpp
+++ sample_code.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class myClass {  // Should be PascalCase
+class MyClass {  // Should be PascalCase
 public:
     int someVariable;    // OK
     int BadVariableName;  // Should be snake_case or camelCase
